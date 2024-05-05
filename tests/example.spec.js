1caf9976b7dd1e5296c880d7c488f917b693f141
Yaroslav Panchishyn <yarpan@gmail.com>
2024-05-05 12:04:55 +0300

    hook fix

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,17 +1,20 @@
-// @ts-nocheck
 const { test, expect } = require('@playwright/test');
 
-let payloadData = process.env.PAYLOAD_DATA;
-let payloadParse = JSON.parse(payloadData);
-let payloadString = JSON.stringify(payloadData);
-let payloadString2 = JSON.stringify(payloadParse);
+// WEBHOOK_PAYLOAD: ${{ toJson(github.event.client_payload) }}
+// WEBHOOK_OBJECT: ${{ toJson(github.event.client_payload.user_data) }}
+
+let payloadData = process.env.WEBHOOK_PAYLOAD;
+let userData = process.env.WEBHOOK_OBJECT; 
+// let userData = JSON.parse(payloadData);
+// let payloadString = JSON.stringify(payloadData);
+// let payloadString2 = JSON.stringify(payloadParse);
 
 test('get started link', async ({ page }) => {
   
-  console.log('=== payloadData ===')
+  console.log('\n=== payloadData ===')
   console.log(payloadData)
-  console.log('=== d ===')
-  console.log(d)
+  console.log('\n=== userData ===')
+  console.log(userData)
 });
 
 
